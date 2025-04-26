b3c21540555ee80ef3a79720e5fb17f73015c9e2
Patcast <patricio.castillo2009@gmail.com>
2025-04-26 15:43:28 +0200

    increased version to

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 NEW_VERSION=$(npm version patch)
 
 # Commit only the package.json file with the version change
-git add package.json
-git commit -m "increased version to ${NEW_VERSION}"
+#git add package.json
+git commit -am "increased version to ${NEW_VERSION}"
 
 git push 
 
